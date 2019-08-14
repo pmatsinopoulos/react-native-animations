3b5cb85daa73933a14e47402d4ce8ef63daa1d0e
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-14 23:38:58 +0300

    We are using Animated

--- App.js
+++ App.js
@@ -6,43 +6,89 @@
  * @flow
  */
 
-import React, { useState } from "react";
-import { Button, Image, LayoutAnimation, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+import { Animated, Button, Easing, Image, StyleSheet, View } from "react-native";
 
 import jake from "./jake.png";
 
-const App = () => {
-  const [position, setPosition] = useState("flex-start");
-  const handlePress = newPosition => {
-    LayoutAnimation.spring();
-    setPosition(newPosition);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spinning: false
+    };
+    this.toggleSpinning = this.toggleSpinning.bind(this);
+    this.getRotationAnimation = this.getRotationAnimation.bind(this);
+    this.startLoopAnimation = this.startLoopAnimation.bind(this);
+    this.stopLoopAnimation = this.stopLoopAnimation.bind(this);
+
+    this._rotationAnimation = new Animated.Value(0);
+    this._rotationOffset = 0;
+  }
+
+  startLoopAnimation = () => {
+    this._rotationAnimation.setOffset(this._rotationOffset);
+    Animated.loop(
+      Animated.timing(this._rotationAnimation, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear
+      })
+    ).start();
   };
 
-  return (
-    <View style={[styles.container, { justifyContent: position }]}>
-      <Image source={jake} />
-      <View style={styles.buttonsContainer}>
-        <Button
-          style={styles.button}
-          title="Top"
-          onPress={() => handlePress("flex-start")}
+  stopLoopAnimation = () => {
+    this._rotationAnimation.stopAnimation(currentValue => {
+      this._rotationOffset = currentValue;
+    });
+  };
+
+  toggleSpinning = () => {
+    this.setState({ spinning: !this.state.spinning }, () => {
+      this.state.spinning
+        ? this.startLoopAnimation()
+        : this.stopLoopAnimation();
+    });
+  };
+
+  getRotationAnimation = () => {
+    const rotate = this._rotationAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
+    });
+    return { rotate };
+  };
+
+  render() {
+    return (
+      <View style={[styles.container]}>
+        <Animated.Image
+          source={jake}
+          style={{ transform: [this.getRotationAnimation()] }}
         />
-        <Button
-          style={styles.button}
-          title="Middle"
-          onPress={() => handlePress("center")}
-        />
-        <Button
-          style={styles.button}
-          title="Bottom"
-          onPress={() => handlePress("flex-end")}
-        />
+        <View style={styles.buttonsContainer}>
+          <Button
+            style={styles.button}
+            title={
+              this.state.spinning ? "Turn Spinning Off" : "Turn Spinning On"
+            }
+            onPress={this.toggleSpinning}
+          />
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    paddingTop: 64,
+    paddingBottom: 32
+  },
   buttonsContainer: {
     flexDirection: "row",
     justifyContent: "space-evenly",
@@ -52,14 +98,6 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-    paddingTop: 64,
-    paddingBottom: 32
   }
 });
 
