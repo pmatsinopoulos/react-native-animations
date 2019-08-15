ffb5311f5961063c32d042feabc4dc55b2adc5d1
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-15 07:38:43 +0300

    Added PanResponder

--- App.js
+++ App.js
@@ -7,7 +7,14 @@
  */
 
 import React, { Component } from "react";
-import { Animated, Button, Easing, Image, StyleSheet, View } from "react-native";
+import {
+  Animated,
+  Button,
+  Easing,
+  PanResponder,
+  StyleSheet,
+  View
+} from "react-native";
 
 import jake from "./jake.png";
 
@@ -24,6 +31,46 @@ class App extends Component {
 
     this._rotationAnimation = new Animated.Value(0);
     this._rotationOffset = 0;
+
+    this._gestureValue = new Animated.ValueXY();
+    this._getstureOffset = { x: 0, y: 0 };
+    this._panResponder = PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+        this._gestureValue.setValue({
+          x: gestureState.dx + this._getstureOffset.x,
+          y: gestureState.dy + this._getstureOffset.y
+        });
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+        this._getstureOffset.x += gestureState.dx;
+        this._getstureOffset.y += gestureState.dy;
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      }
+    });
   }
 
   startLoopAnimation = () => {
@@ -64,7 +111,14 @@ class App extends Component {
       <View style={[styles.container]}>
         <Animated.Image
           source={jake}
-          style={{ transform: [this.getRotationAnimation()] }}
+          style={{
+            transform: [
+              this.getRotationAnimation(),
+              { translateX: this._gestureValue.x },
+              { translateY: this._gestureValue.y }
+            ]
+          }}
+          {...this._panResponder.panHandlers}
         />
         <View style={styles.buttonsContainer}>
           <Button
